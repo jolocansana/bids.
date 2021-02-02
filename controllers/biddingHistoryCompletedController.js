5ab9a38e22ed082d42689ffdc2676aa016bfa64a
Trisha <48178740+trishapelagio@users.noreply.github.com>
2021-02-02 17:48:41 +0800

    updated history

--- controllers/biddingHistoryCompletedController.js
+++ controllers/biddingHistoryCompletedController.js
@@ -1,5 +1,6 @@
 const db = require("../models/db.js");
 const Listing = require("../models/ListingModel.js");
+const Participation = require("../models/ParticipationModel.js");
 const nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
@@ -12,7 +13,37 @@ var transporter = nodemailer.createTransport({
 
 const completedController = {
     getCompleted: function(req, res) {
-		res.render('bidding-history-completed')
+		var userEmail = req.session.email
+		db.findMany(Participation, {email:userEmail, status:'won'}, null, function(result) {
+			var ids = []
+			for(var i=0; i<result.length; i++)
+				ids.push(result[i].listingId)
+				
+			ids = [...new Set(ids)]
+			
+			db.findMany(Listing, {_id:ids}, null, function(results) {
+				var orders = []
+				for (var i=0; i<results.length ; i++){ 
+					var image = results[i].images[0]
+					var name = results[i].name
+					var highestBid = results[i].highestBid
+					var highestBidder = results[i].highestBidder
+					var endDate = results[i].endDate.toDateString()
+					var _id = results[i]._id
+					var order = {
+						image: image,
+						name: name,
+						highestBid:highestBid,
+						highestBidder:highestBidder,
+						endDate:endDate,
+						_id:_id
+					}
+					orders.push(order)
+				}
+				res.render('bidding-history-completed', {orders:orders})
+			})
+		})	
+		
 	},
 
 	report: function(req, res){
