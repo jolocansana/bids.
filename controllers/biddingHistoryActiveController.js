bd5a7e092cc3e1df620a09659ccf1ad98204d46e
Trisha <48178740+trishapelagio@users.noreply.github.com>
2021-02-07 14:04:30 +0800

    Fixed start date issue

--- controllers/biddingHistoryActiveController.js
+++ controllers/biddingHistoryActiveController.js
@@ -11,7 +11,7 @@ const activeController = {
 				ids.push(result[i].listingId)
 				
 			ids = [...new Set(ids)]
-			
+
 			db.findMany(Listing, {_id:ids}, null, function(results) {
 				var orders = []
 				for (var i=0; i<results.length ; i++){ 
@@ -21,15 +21,13 @@ const activeController = {
 					var highestBid = results[i].highestBid
 					var highestBidder = results[i].highestBidder
 					var endDate = results[i].endDate.toString()
-					var remaining = Math.abs(new Date() - new Date(endDate.replace(/-/g,'/')));
-					var remaining = msToTime(remaining)
 					var _id = results[i]._id
 					var order = {
 						image: image,
 						name: name,
 						highestBid:highestBid,
 						highestBidder:highestBidder,
-						remaining:remaining,
+						endDate:endDate,
 						_id:_id
 					}
 					orders.push(order)
@@ -37,43 +35,7 @@ const activeController = {
 				res.render('bidding-history-active', {orders:orders})
 			})
 		})	
-	}, 
-	getActiveTimes: function(req, res){
-		var useremail = req.session.email
-		db.findMany(Participation, {email:userEmail, status:'active'}, null, function(result) {
-			var ids = []
-			for(var i=0; i<result.length; i++)
-				ids.push(result[i].listingId)
-				
-			ids = [...new Set(ids)]
-			
-			db.findMany(Listing, {_id:ids}, {}, function(results) {
-				var orders = []
-				for (var i=0; i<results.length ; i++){ 
-
-					var image = results[i].images[0]
-					var name = results[i].name
-					var highestBid = results[i].highestBid
-					var highestBidder = results[i].highestBidder
-					var endDate = results[i].endDate.toString()
-					var remaining = Math.abs(new Date() - new Date(endDate.replace(/-/g,'/')));
-					var remaining = msToTime(remaining)
-					var _id = results[i]._id
-					var order = {
-						image: image,
-						name: name,
-						highestBid:highestBid,
-						highestBidder:highestBidder,
-						remaining:remaining,
-						_id:_id
-					}
-					orders.push(order)
-				}
-				res.send(orders).status(200)
-			})
-		})	
-		
-	},
+	}
 }
 
 function msToTime(duration) {
