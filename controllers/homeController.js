f320066bad8f1f4615133331d90af363d6eebc7d
Tony Ramirez <39904525+bryceramirez@users.noreply.github.com>
2021-02-07 16:34:37 +0800

    highest bidder ok

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -19,74 +19,78 @@ const homeController = {
 					const findListing =  Listing.findOne({
 						_id: listID
 					})
+
+					db.findOne(Listing,{_id: listID},{}, function(findListing){
+						if (findListing.highestBidderId != undefined) {
+							// Listing.updateOne({
+							// 	_id: listID
+							// }, {
+							// 	status: 'inactive',
+							// 	soldToUser: findListing.highestBidderId
+							// });
+							db.updateOne(Listing, { _id: listID }, { status: "inactive", soldToUser: findListing.highestBidderId });
+
+						}
+						else {
+							// Listing.updateOne({
+							// 	_id: listID
+							// }, {
+							// 	status: 'inactive'
+							// });
+							db.updateOne(Listing, { _id: listID }, { status: "inactive" });
+						}
+
+
+						//  Win notification
+
+						var winUserID = findListing.highestBidderId;
+						var listingID = listID
+						var date = Date.now();
+
+
+						db.updateOne(Participation, { listingId: listingID, "user._id": winUserID }, { status: 'won' });
+
+						db.updateMany(Participation, { listingId: listingID, "user._id": { $ne: winUserID } }, { status: "lost" });
+
+
+						db.findOne(Listing, { _id: listingID }, {}, function (listing) {
+							var description = "You won the " + listing.name + " bid for " + listing.highestBid;
+							var notif = {
+								userID: winUserID,
+								listingID: listingID,
+								description: description,
+								date: date
+							};
+
+							db.insertOne(Notification, notif, function (result) {
+
+							});
+						});
+
+						// Losers notification
+						db.findOne(Listing, { _id: listingID }, {}, function (result) {
+							db.findMany(Participation, { listingId: listingID }, {}, function (listing) {
+								for (var i = 0; i < listing.length; i++) {
+									if (String(listing[i].user._id) != String(winUserID)) {
+										var description = "You lost the " + result.name + " bid";
+										var notif = {
+											userID: listing[i].user._id,
+											listingID: listingID,
+											description: description,
+											date: date
+										};
+
+										db.insertOne(Notification, notif, function (result) {
+
+										});
+									}
+								}
+							});
+						});
+					});
 					console.log("highestbidder: " + findListing);
 
-					if(findListing.highestBidderId != undefined){
-						// Listing.updateOne({
-						// 	_id: listID
-						// }, {
-						// 	status: 'inactive',
-						// 	soldToUser: findListing.highestBidderId
-						// });
-						db.updateOne(Listing, {_id: listID}, {status: "inactive", soldToUser:findListing.highestBidderId});
-
-					}
-					else {
-						// Listing.updateOne({
-						// 	_id: listID
-						// }, {
-						// 	status: 'inactive'
-						// });
-						db.updateOne(Listing, {_id: listID}, {status: "inactive"});
-					}
 					
-
-					//  Win notification
-					
-					var winUserID = findListing.highestBidderId;
-					var listingID = listID
-					var date = Date.now();
-
-					
-					db.updateOne(Participation, { listingId: listingID, _id: winUserID }, { status: 'won' });
-
-					db.updateOne(Participation, { listingId: listingID, _id: { $ne: winUserID} }, {status: "lost"});
-				
-
-					db.findOne(Listing, { _id: listingID }, {}, function (listing) {
-						var description = "You won the " + listing.name + " bid for " + listing.highestBid;
-						var notif = {
-							userID: winUserID,
-							listingID: listingID,
-							description: description,
-							date: date
-						};
-
-						db.insertOne(Notification, notif, function (result) {
-
-						});
-					});
-
-					// Losers notification
-					db.findOne(Listing, { _id: listingID }, {}, function (result) {
-						db.findMany(Participation, { listingId: listingID }, {}, function (listing) {
-							for (var i = 0; i < listing.length; i++) {
-								if (String(listing[i].user._id) != String(winUserID)) {
-									var description = "You lost the " + result.name + " bid";
-									var notif = {
-										userID: listing[i].user._id,
-										listingID: listingID,
-										description: description,
-										date: date
-									};
-
-									db.insertOne(Notification, notif, function (result) {
-
-									});
-								}
-							}
-						});
-					});
 					
 					
 				}
